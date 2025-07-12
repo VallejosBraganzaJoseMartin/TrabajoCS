a82c56159daac99e8a3ef9cbf3e52ec853236cc2
Jose <jmvallejosb@utn.edu.ec>
2025-07-12 00:00:21 -0500

    Menu

--- client/src/pages/MenuPage.jsx
+++ client/src/pages/MenuPage.jsx
@@ -30,7 +30,7 @@ const MenuPage = () => {
   };
 
   const getDefaultImage = () => {
-    return "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=481&q=80";
+    return "https://dynamoprojects.com/wp-content/uploads/2022/12/no-image.jpg";
   };
 
   // Adaptar los datos de la pizza a lo que espera el modal
